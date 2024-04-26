a0be479521afe9f6b580618848836bba6120d4a0
aredhachairman <152467918+aredhachairman@users.noreply.github.com>
2024-04-26 13:51:13 +0300

    Create colours.dart
    Background, text and button colors

---
+++ constants/colours.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color.fromARGB(255, 130, 24, 24);
+const Color appWhiteColor = Color(0xFFFFFFFF);
+const Color appBlackColor = Color(0xFF000000);
+const Color appGrayColor = Color.fromARGB(255, 179, 68, 68);
